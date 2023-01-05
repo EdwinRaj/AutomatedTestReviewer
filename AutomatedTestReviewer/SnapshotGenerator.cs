50548d639bf02ee97f1da9f8b836338c0a40ebd6
Edwin Raj <edwin.be@gmail.com>
2023-01-05 15:36:59 +0000

    Removed the image comparer

--- AutomatedTestReviewer/SnapshotGenerator.cs
+++ AutomatedTestReviewer/SnapshotGenerator.cs
@@ -1,13 +1,4 @@
-﻿using GroupDocs.Comparison;
-using GroupDocs.Comparison.Options;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Timers;
 
 namespace AutomatedTestReviewer
